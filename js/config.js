cd3c1ee709f3b7c5173ab451fdd352dae04bc23e
taohz <leonardocorso17@gmail.com>
2017-05-10 00:15:29 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "ACE .png";
+var l_serverImage = "ACE.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
