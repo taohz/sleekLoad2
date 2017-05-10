15c7ee5abac8f35d4ef3abfd92aef4b20bb88e11
taohz <leonardocorso17@gmail.com>
2017-05-09 23:55:45 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "images/logo.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "sleekLoad 2";
+var l_serverName = "ACERP";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
