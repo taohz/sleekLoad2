78b5c8b0bee9748c53a56c00b7f7495e045e6d50
taohz <leonardocorso17@gmail.com>
2017-05-10 00:01:17 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -44,7 +44,7 @@ Avaiable options:
 
 You can add custom backgrounds
 Just make sure it is at webm VP8 format */
-var l_background = "hues.webm";
+var l_background = "backgrounds/videos/hues.webm";
 
 /*(Image-background only)
 Place one or more images
