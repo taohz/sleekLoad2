ee31ffb7e3f2908f95e061f3ee89ea6e93380119
taohz <leonardocorso17@gmail.com>
2017-05-10 00:02:42 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@
 
 /*URL for the server logo
 Leave it blank to use text instead*/
-var l_serverImage = "images/logo.png";
+var l_serverImage = "acerp logo_1492895494.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
