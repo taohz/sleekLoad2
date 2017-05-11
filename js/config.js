2e750225e480a8ea1719f0b94f1de468ebd47810
taohz <leonardocorso17@gmail.com>
2017-05-10 17:25:36 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "ACERP";
+var l_serverName = "";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
